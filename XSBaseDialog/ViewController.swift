259abe238ec59be8b2c55f448803cb36f7106ca8
黄学松 <huangxuesong@jekunauto.com>
2021-03-18 16:06:56 +0800

    Initial Commit

---
+++ XSBaseDialog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XSBaseDialog
+//
+//  Created by ZB-YF-HXS on 2021/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
